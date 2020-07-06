0a7ff96832995748a0399ad838eec413d39eabe9
Chris Putnam <chris@webfx.com>
2020-07-06 07:38:34 -0400

    Beginning to build command substructure

---
+++ src/command_abstract.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Command abstract
+ *  - primary or subcommand structure
+ *  - main "run" method accepts and parses arguments
+ *  - default run method checks for available sub-methods
+ */
+
+class Command_Abstract
+{
+}
