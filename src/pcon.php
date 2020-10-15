9940c2079adb06f0ff4715da98fb441a824ccc76
Chris Putnam <chris@webfx.com>
2020-10-15 07:37:55 -0400

    Version bump - 1.3

--- src/pcon.php
+++ src/pcon.php
@@ -6,7 +6,7 @@
 Class PCon extends Console_Abstract
 {
 
-    const VERSION = "1.2.0";
+    const VERSION = "1.3.0";
     // Name of script and directory to store config
     const SHORTNAME = 'pcon';
 
