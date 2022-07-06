9514658b4ab3978c8fb8768147eb38ad08a5225c
Chris Putnam <chris@webfx.com>
2022-07-06 07:20:23 -0400

    In progress - PHPDoc

--- phpdoc.sh
+++ phpdoc.sh
@@ -4,7 +4,13 @@ clear
 
 # cp -f pcon pcon.php
 
+rm -rf docs/*
+
 phpDocumentor -d . -t docs
+
+if [ "$1" == "-o" ]; then
+    google-chrome "file:///home/chris/dev/personal/pcon/docs/index.html"
+fi
 
 # && rm pcon.php && exit 0
 
